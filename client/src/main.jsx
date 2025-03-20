7ed9b263026fe237b9eedb978a01237e9e9d7b55
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-03-20 01:56:37 +0000

    removing unnecessery files from both client and server sides

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import User from './User';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/user" element={<User />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  </StrictMode>,
+)
