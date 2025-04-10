b42e1284d0c8807a993bfb2b406867b37c5d0db3
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-10 18:37:19 +0100

    finishing insidePlayers Factory

--- server/database/factories/Users/InsidePlayerFactory.php
+++ server/database/factories/Users/InsidePlayerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories\Users;
 
+use App\Models\Users\Player;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,13 @@ class InsidePlayerFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "pace" => $this->faker->numberBetween(50, 99),
+            "dribbling" => $this->faker->numberBetween(50,99),
+            "shooting" => $this->faker->numberBetween(50,99),
+            "defending" => $this->faker->numberBetween(50,99),
+            "passing" => $this->faker->numberBetween(50,99),
+            "physical" => $this->faker->numberBetween(50,99),
+            "player_id" => Player::inRandomOrder()->first()->id ? : Player::factory()->create()->id,  
         ];
     }
 }
