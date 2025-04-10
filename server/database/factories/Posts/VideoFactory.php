14cb21f73cf595a392b0fce0d18cf3e01fbf0dde
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-10 20:20:18 +0100

    finishingVideoFactory

--- server/database/factories/Posts/VideoFactory.php
+++ server/database/factories/Posts/VideoFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Posts\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class VideoFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "intitule_video" => $this->faker->sentence(3),
+            "description" => $this->faker->paragraph(3),
+            "post_id" => Post::inRandmOrder()->first()->id ?? Post::factory()->create()->id,
         ];
     }
 }
