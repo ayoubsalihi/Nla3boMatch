e23ba2fb982179348a0f4582f7be73d8cde6fc6f
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-18 17:14:52 +0100

    Set the Terrain side on DatabaseSeeder

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\Terrains\AcademySeeder;
+use Database\Seeders\Terrains\ReservationSeeder;
+use Database\Seeders\Terrains\TerrainSeeder;
 use Database\Seeders\Users\AdminSeeder;
 use Database\Seeders\Users\ChatSeeder;
 use Database\Seeders\Users\FriendshipSeeder;
@@ -38,6 +41,12 @@ class DatabaseSeeder extends Seeder
             FriendshipSeeder::class,
             ChatSeeder::class,
             AdminSeeder::class,
+        ]);
+
+        $this->call([
+            TerrainSeeder::class,
+            ReservationSeeder::class,
+            AcademySeeder::class,
         ]);
     }
 }
