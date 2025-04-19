9d63429372489d57fe3c8598f5f2ad24662a90d0
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-19 21:05:01 +0100

    finishing MessageTest successfully

--- server/app/Models/Teams/TeamChat.php
+++ server/app/Models/Teams/TeamChat.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Teams;
 
+use App\Models\Users\Message;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,9 @@ class TeamChat extends Model
     // each team chat belongs to a specific team
     public function team(){
         return $this->hasOne(Team::class);
+    }
+
+    public function messages(){
+        return $this->hasMany(Message::class);
     }
 }
