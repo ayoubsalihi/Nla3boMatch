5f2f9db4f65c7e1a0e9adca859e3394fb86bcd61
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-20 22:13:08 +0100

    Finished StoreUserRequest

---
+++ server/app/Http/Requests/Users/StoreUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Users;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "email" => ["required","email"],
+            "password" => ["required","string","min:8","confirmed"],
+            "nom" => ["required","string"],
+            "prenom" => ["required","string"],
+            "cin" => ["required","string"],
+            "type_utilisateur" => ["required", "string", "in:admin,player"],
+            "telephone" => ["required","string"],
+            "ville_residence" => ["required","string"],
+            "quartier" => ["required","string"],
+        ];
+    }
+}
