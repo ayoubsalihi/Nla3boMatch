1861d0448eb466bed930c5d1db3d52ea262a3de4
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-20 22:12:25 +0100

    Finished StoreUserRequest

--- server/app/Http/Controllers/Users/UserController.php
+++ server/app/Http/Controllers/Users/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\Users\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json($users);
     }
 
     /**
@@ -20,7 +22,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
