f9c8f07a8f6de5b6211a90dfd6075252d9a93f6b
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-21 19:36:56 +0100

    Finished PlayerController successfully

--- server/app/Http/Controllers/Users/PlayerController.php
+++ server/app/Http/Controllers/Users/PlayerController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Users\StorePlayerRequest;
+use App\Http\Requests\Users\UpdatePlayerRequest;
+use App\Models\Users\Player;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller
@@ -12,38 +15,50 @@ class PlayerController extends Controller
      */
     public function index()
     {
-        //
+        $players = Player::all();
+        return response()->json($players);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePlayerRequest $request)
     {
-        //
+        $player = Player::create($request->validated());
+        return response()->json([
+            "message" => "Player created successfully",
+            "player" => $player,
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Player $player)
     {
-        //
+        return response()->json($player);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdatePlayerRequest $request, Player $player)
     {
-        //
+        $player->update($request->all());
+        return response()->json([
+            "message" => "Player have been updated successfully",
+            "player" => $player,
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Player $player)
     {
-        //
+        $player->delete();
+        return response()->json([
+            "message" => "Player deleted successfully",
+        ]);
     }
 }
