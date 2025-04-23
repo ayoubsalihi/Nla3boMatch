d0d2e5f94429a4ec66121ea51048ca7fb033f85d
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-23 06:17:08 +0100

    Finished InsidePlayerController su_ccessfully

--- server/app/Http/Controllers/Users/InsidePlayerController.php
+++ server/app/Http/Controllers/Users/InsidePlayerController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateInsidePlayerRequest;
+use App\Http\Requests\Users\StoreInsidePlayerRequest;
+use App\Models\Users\InsidePlayer;
 use Illuminate\Http\Request;
 
 class InsidePlayerController extends Controller
@@ -12,38 +15,49 @@ class InsidePlayerController extends Controller
      */
     public function index()
     {
-        //
+        $InsidePlayer = InsidePlayer::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreInsidePlayerRequest $request)
     {
-        //
+        $InsidePlayer = InsidePlayer::create($request->validated());
+        return response()->json([
+            "message" => "InsidePlayer created successfully",
+            "InsidePlayer" => $InsidePlayer,
+        ],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(InsidePlayer $InsidePlayer)
     {
-        //
+        return response()->json($InsidePlayer);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateInsidePlayerRequest $request,InsidePlayer $InsidePlayer)
     {
-        //
+        $InsidePlayer->update($request->validated());
+        return response()->json([
+            "message" => 'InsidePleyer updated successfully',
+            "InsidePlayer" => $InsidePlayer,
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(InsidePlayer $InsidePlayer)
     {
-        //
+        $InsidePlayer->delete();
+        return response()->json([
+            "message" => 'InsidePlayer deleted successfully'
+        ]);
     }
 }
