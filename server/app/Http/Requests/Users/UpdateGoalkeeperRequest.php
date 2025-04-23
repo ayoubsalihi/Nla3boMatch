8992cdbad94d20435dc7dccf6d549ad0751b9014
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-23 18:05:39 +0100

    Finishing UpdategoalkeeperResuest

---
+++ server/app/Http/Requests/Users/UpdateGoalkeeperRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Users;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateGoalkeeperRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "diving" => ["required", "integer", "between:0,100"],
+            "reflex" => ["required","integer","between:0,100"],
+            "kicking" => ["required","integer","between:0,100"],
+            "handling" => ["required","integer","between:0,100"],
+            "positionning" => ["required","integer","between:0,100"],
+            "speed" => ["required","integer","between:0,100"],
+        ];
+    }
+}
