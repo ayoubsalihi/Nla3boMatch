4a2e54f8fe1ada2c4d8a814bef193f5e6e6f0715
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-26 11:00:09 +0100

    Finishing everything about Team

--- server/app/Http/Controllers/Teams/TeamController.php
+++ server/app/Http/Controllers/Teams/TeamController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Teams;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Teams\StoreTeamRequest;
+use App\Http\Requests\Teams\UpdateTeamRequest;
+use App\Models\Teams\Team;
 use Illuminate\Http\Request;
 
 class TeamController extends Controller
@@ -12,38 +15,50 @@ class TeamController extends Controller
      */
     public function index()
     {
-        //
+        $teams = Team::all();
+        return response()->json($teams);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTeamRequest $request)
     {
-        //
+        $team->create($request->validated());
+        return response()->json([
+            "message" => "Team created successfully",
+            "team" => $team
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Team $team)
     {
-        //
+        return response()->json($team);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateTeamRequest $request, Team $team)
     {
-        //
+        $team->update($request->validated());
+        return response()->json([
+            "message" => "Team updated successfully",
+            "team" => $team
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Team $team)
     {
-        //
+        $team->delete();
+        return response()->json([
+            "message" => "Team deleted successfully",
+        ]);
     }
 }
