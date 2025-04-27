22e0ef02e0271d7de3019786e195f0ab3078a616
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-04-27 22:43:40 +0100

    fix the issue #21 and refactoring the PartidoTeamController

--- server/app/Http/Controllers/Competitions/PartidoTeamController.php
+++ server/app/Http/Controllers/Competitions/PartidoTeamController.php
@@ -5,39 +5,36 @@ namespace App\Http\Controllers\Competitions;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Competitions\StorePartidoTeamRequest;
 use App\Http\Requests\Competitions\UpdatePartidoTeamRequest;
-use App\Models\Competitions\Compete;
+use App\Models\Competitions\PartidoTeam;
 
 class PartidoTeamController extends Controller
 {
     public function index()
     {
-        $records = Compete::all();
+        $records = PartidoTeam::all();
         return response()->json($records);
     }
 
-    public function show($id)
+    public function show(PartidoTeam $partidoTeam)
     {
-        $record = Compete::findOrFail($id);
-        return response()->json($record);
+        return response()->json($partidoTeam);
     }
 
     public function store(StorePartidoTeamRequest $request)
     {
-        $record = Compete::create($request->validated());
+        $record = PartidoTeam::create($request->validated());
         return response()->json(['message' => 'Équipe ajoutée au match avec succès.','data' => $record]);
     }
 
-    public function update(UpdatePartidoTeamRequest $request, $id)
+    public function update(UpdatePartidoTeamRequest $request, PartidoTeam $partidoTeam)
     {
-        $record = Compete::findOrFail($id);
-        $record->update($request->validated()); 
-        return response()->json(['message' => 'Relation match-équipe mise à jour avec succès.','data' => $record]);
+        $partidoTeam->update($request->validated()); 
+        return response()->json(['message' => 'Relation match-équipe mise à jour avec succès.','data' => $partidoTeam]);
     }
 
-    public function destroy($id)
+    public function destroy(PartidoTeam $partidoTeam)
     {
-        $record = Compete::findOrFail($id);
-        $record->delete();
+        $partidoTeam->delete();
         return response()->json(['message' => 'Relation match-équipe supprimée avec succès.']);
     }
 }
