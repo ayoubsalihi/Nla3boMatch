98447dfdaa20fee25f0d7f2c0b7aba4b81ec697e
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-05-07 17:36:26 +0100

    Main component finished using routes structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import store from './redux/store'
+import React from 'react'
+import App from './App'
+import Unauthorized from './components/Errors/ErrorBoundary'
+import ReactDOM from 'react-dom/client'
+import Login from './components/Errors/Login'
+import HeroPage from './components/Public/HeroPage'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children:[
+      {index:true, element: <HeroPage /> },
+      {path:'login',element:<Login/>},
+      {path:'unauthorized',element:<Unauthorized/>},
+      {}
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 )
