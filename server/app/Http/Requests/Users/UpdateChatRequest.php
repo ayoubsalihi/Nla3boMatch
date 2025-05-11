c0640bf606f0dfe3fad6e3a20e5924c464dd8cb0
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-05-11 19:46:43 +0100

    Fixing some problems related to psr-4 loading

--- server/app/Http/Requests/Users/UpdateChatRequest.php
+++ server/app/Http/Requests/Users/UpdateChatRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\User;
+namespace App\Http\Requests\Users;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -11,7 +11,7 @@ class UpdateChatRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
