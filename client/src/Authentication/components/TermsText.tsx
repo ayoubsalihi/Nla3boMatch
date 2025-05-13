bb1cbc22dc3cabe221564b179243f8278ffa970d
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-05-13 12:17:43 +0100

    Terms with sign in button components

---
+++ client/src/Authentication/components/TermsText.tsx
@@ -0,0 +1,16 @@
+const TermsText = () => {
+  return (
+    <p className="mt-6 text-xs text-gray-600 text-center">
+      I agree to abide by Cartesian Kinetics{" "}
+      <a href="#" className="border-b border-gray-500 border-dotted">
+        Terms of Service
+      </a>{" "}
+      and its{" "}
+      <a href="#" className="border-b border-gray-500 border-dotted">
+        Privacy Policy
+      </a>
+    </p>
+  );
+};
+
+export default TermsText;
