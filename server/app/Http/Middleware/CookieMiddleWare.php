20252fab4c3520962b0a7b685a4b42f529d5434a
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-05-21 17:34:59 +0100

    Refactoring login controller and cookie middleware

--- server/app/Http/Middleware/CookieMiddleWare.php
+++ server/app/Http/Middleware/CookieMiddleWare.php
@@ -15,9 +15,15 @@ class CookieMiddleWare
      */
     public function handle(Request $request, Closure $next): Response
     {
+         $response = $next($request);
+        
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Allow-Origin', config('cors.allowed_origins'));
+        
         if ($token = $request->cookie('auth_token')) {
             $request->headers->set('Authorization', "Bearer $token");
         }
-        return $next($request);
+        
+        return $response;
     }
 }
