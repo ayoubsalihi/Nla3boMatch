b7c2133ebae8b741f488fe58e19b2cc38dffb7a4
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-05-24 10:38:56 +0100

    Fixing the register request and action

--- server/app/Http/Requests/Auth/RegisterRequest.php
+++ server/app/Http/Requests/Auth/RegisterRequest.php
@@ -20,34 +20,39 @@ class RegisterRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
-    {
-        return [
-            "nom" => ["required", "string", "max:255"],
-            "prenom" => ["required", "string", "max:255"],
-            "email" => ["required", "string", "email", "max:255", "unique:users"],
-            "password" => ["required", "string", "min:8", "confirmed"],
-            "cin" => ["required", "string", "max:255", "unique:users"],
-            "type_utilisateur" => ["required", "string", "in:player,admin"],
-            "telephone" => ["required", "string", "regex:/^\+212[0-9]{9}$/"],
-            "ville_residence" => ["required", "string", "max:255"],
-            "quartier" => ["required", "string", "max:255"],
-            "poste" => ["required_if:type_utilisateur,player", "string", "max:255"],
-            
-            // Goalkeeper specific
-            "diving" => ["required_if:poste,GK", "integer", "between:0,100"],
-            "reflex" => ["required_if:poste,GK", "integer", "between:0,100"],
-            "handling" => ["required_if:poste,GK", "integer", "between:0,100"],
-            "kicking" => ["required_if:poste,GK", "integer", "between:0,100"],
-            "positionning" => ["required_if:poste,GK", "integer", "between:0,100"],
-            "speed" => ["required_if:poste,GK", "integer", "between:0,100"],
-            
+{
+    $rules = [
+        "nom" => "required|string|max:255",
+        "prenom" => "required|string|max:255",
+        "email" => "required|string|email|max:255|unique:users",
+        "password" => "required|string|min:8|confirmed",
+        "cin" => "required|string|max:255|unique:users",
+        "type_utilisateur" => "required|string|in:player,admin",
+        "telephone" => "required|string|regex:/^\+212[0-9]{9}$/",
+        "ville_residence" => "required|string|max:255",
+        "quartier" => "required|string|max:255",
+    ];
+
+    if ($this->input('type_utilisateur') === 'player') {
+        $rules = array_merge($rules, [
+            "poste" => "required|string|in:GK,DEF,MF,FW",
+            // Goalkeeper-specific
+            "diving" => "required_if:poste,GK|integer|between:0,100",
+            "reflex" => "required_if:poste,GK|integer|between:0,100",
+            "handling" => "required_if:poste,GK|integer|between:0,100",
+            "kicking" => "required_if:poste,GK|integer|between:0,100",
+            "positionning" => "required_if:poste,GK|integer|between:0,100",
+            "speed" => "required_if:poste,GK|integer|between:0,100",
             // Field player
-            "pace" => ["required_unless:poste,GK", "integer", "between:0,100"],
-            "dribbling" => ["required_unless:poste,GK", "integer", "between:0,100"],
-            "shooting" => ["required_unless:poste,GK", "integer", "between:0,100"],
-            "defending" => ["required_unless:poste,GK", "integer", "between:0,100"],
-            "passing" => ["required_unless:poste,GK", "integer", "between:0,100"],
-            "physical" => ["required_unless:poste,GK", "integer", "between:0,100"],
-        ];
+            "pace" => "required_unless:poste,GK|integer|between:0,100",
+            "dribbling" => "required_unless:poste,GK|integer|between:0,100",
+            "shooting" => "required_unless:poste,GK|integer|between:0,100",
+            "defending" => "required_unless:poste,GK|integer|between:0,100",
+            "passing" => "required_unless:poste,GK|integer|between:0,100",
+            "physical" => "required_unless:poste,GK|integer|between:0,100",
+        ]);
     }
+
+    return $rules;
+}
 }
