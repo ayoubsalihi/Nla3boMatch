5b92aaac281a8cae8dfc8fb462a231fd507d5869
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2025-05-25 11:55:37 +0100

    Animated delete icon from freepik for the competition

---
+++ client/src/components/Public/Components/Dashboard/Admin/Competitions/Details/components/Animations/Delete.tsx
@@ -0,0 +1,13 @@
+import Lottie from "lottie-react"
+import DeleteIcon from "../Animations/Json/Delete.json"
+
+
+const Delete = () => {
+  return (
+    <div>
+        <Lottie animationData={DeleteIcon} loop={true}/>
+    </div>
+  )
+}
+
+export default Delete
